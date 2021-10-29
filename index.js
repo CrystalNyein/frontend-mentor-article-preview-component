748ae782701ff4a634e789752ebeddfe58d335e6
CrystalNyein <nyeinchanlay99@gmail.com>
2021-10-29 16:12:19 +0630

    Tablets and mobile styles done

--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const shareSocial = (e) => {
   let rect = e.target.getBoundingClientRect();
   let top = e.target.offsetTop;
   let left = e.target.offsetLeft;
-  console.log(top, left);
   const shareContainer = document.querySelector(".share-container");
+  const cardSocial = document.querySelector(".card-social");
+  const shareActive = document.getElementById("share-icon");
+  const card = document.querySelector(".card");
   if (shareContainer.classList.contains("display-none")) {
     e.target.classList.add("share-active");
+    cardSocial.classList.add("display-mobile-none");
     shareContainer.classList.remove("display-none");
     shareContainer.style.bottom = rect.top - 93 + "px";
-    shareContainer.style.left = rect.left - 10 * 13 - 10 + "px";
+    shareContainer.style.left = rect.left - 10 * 13 - 8 + "px";
+    card.classList.add("card-radius");
   } else {
-    e.target.classList.remove("share-active");
+    shareActive.classList.remove("share-active");
     shareContainer.classList.add("display-none");
+    cardSocial.classList.remove("display-mobile-none");
+    card.classList.remove("card-radius");
   }
 };
